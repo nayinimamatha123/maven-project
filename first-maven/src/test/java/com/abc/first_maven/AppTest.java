4c6380c3e103bfb73241e01514ae4f9cdb359ba0
Mamatha Nayini <Mamatha Nayini@LAPTOP-D4DGKQ8H>
2020-06-30 10:42:00 +0530

    first commit

---
+++ first-maven/src/test/java/com/abc/first_maven/AppTest.java
@@ -0,0 +1,24 @@
+package com.abc.first_maven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public static void main(String[] args)
+    {
+    	System.out.println("text is completed");
+    }
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
